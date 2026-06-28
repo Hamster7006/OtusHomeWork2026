3dce3629512e442d86c356ef13495d6999c40ced
Hamster7006 <82329588+Hamster7006@users.noreply.github.com>
2026-06-28 13:15:48 +0400

    Add project files.

---
+++ OtusHomeWork2026/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OtusHomeWork2026
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
