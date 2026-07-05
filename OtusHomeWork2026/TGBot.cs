b9754cf99740ded2cd47d88987ac549a52151edd
Hamster7006 <82329588+Hamster7006@users.noreply.github.com>
2026-07-05 17:15:14 +0400

    25 Различные виды клавиатур // ДЗ

--- OtusHomeWork2026/TGBot.cs
+++ OtusHomeWork2026/TGBot.cs
@@ -18,16 +18,23 @@ namespace OtusHomeWork2026
     {
         public async Task StartTGBotAsync()
         {
+            #region Получение токена
+            string token = string.Empty;
+
+            // получение токена ТГ из переменной
             //string? token = Environment.GetEnvironmentVariable("TelegramBotTokenOTUSBasic", EnvironmentVariableTarget.User);
 
-            string token = string.Empty;
-            //token = GetTokenTgBot();
+            //получение токена ТГ из консоли
+            //Console.WriteLine("Ведите токен для ТГ бота");
+            //token = Console.ReadLine();
 
+            //получение токена ТГ из файла
             using (StreamReader reader = new StreamReader("C:\\Users\\Alkesandr\\Desktop\\tgtoken.txt"))
             {
-                token = await reader.ReadToEndAsync();
+                token = reader.ReadToEnd();
                 Console.WriteLine(token);
             }
+            #endregion Получение токена
 
             try
             {
